eebce7f084c35c068d117f941a6d4c57abdac45d
Manash07 <72068939+Manash07@users.noreply.github.com>
2023-04-11 07:43:21 +0545

    comments written

--- manash.js
+++ manash.js
@@ -1,5 +1,14 @@
 
 const name = () =>{
-    const manash = "Manash"   
+    const manash = "Manash" 
+    console.log(manash)  
 }
-console.log("Manash")
+
+// git fetch => to know updates
+
+
+/** error: Your local changes to the following files would be overwritten by checkout:
+        manash.js
+Please commit your changes or stash them before you switch branches. */
+
+// git stash => if changed code is not needed else git acp
